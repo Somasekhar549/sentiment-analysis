8c4535742ec913b859a2b39786846f7a867d57f0
gsomasekhar005@gmail.com <48669216+Somasekhar549@users.noreply.github.com>
2022-05-15 23:19:59 +0530

    Update SentimentAnalysis.py

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -2,37 +2,16 @@ import streamlit as st
 import pandas as pd
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+
 import os
 import re
-from tqdm import tqdm
+
 import wordninja
 import unidecode
 import pickle
-from bs4 import BeautifulSoup
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score
-from gensim.models import Word2Vec
-from gensim.models import KeyedVectors
+f
 
 
-from sklearn.model_selection import RandomizedSearchCV
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.calibration import CalibratedClassifierCV
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import log_loss
-from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
 
 
 
